02a983025ee4e8b7c33a8269ed9fba971fd032f5
Felix <felixgerick@gmail.com>
2023-02-08 15:16:25 +0100

    open ppt on mac, not linux

--- src/write.jl
+++ src/write.jl
@@ -168,7 +168,13 @@ function Base.write(
         cd(origin)
     end
     if open_ppt
-        run(`cmd /C start powerpnt.exe /C $filepath`)
+        if Sys.iswindows()
+            run(`cmd /C start powerpnt.exe /C $filepath`)
+        elseif Sys.isapple()
+            run(`open $filepath`)
+        else
+            warn("Open .pptx files not implemented on your OS")
+        end
     end
     return nothing
 end
